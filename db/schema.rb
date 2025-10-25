21e0ff2991ac71d3306a0a290bce831bc3443060
nkkn331 <232537865+nkkn331@users.noreply.github.com>
2025-10-25 15:48:55 +0900

    11-10画像投稿「詳細ページに追記」まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_23_070619) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_25_044158) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
     t.string "text"
+    t.string "image_id"
   end
 
 end
