e4c42b903cb746b7aaebaf851de5a37d52f6808a
nkkn331 <232537865+nkkn331@users.noreply.github.com>
2026-01-16 13:49:58 +0900

    create viewcount table

---
+++ db/migrate/20260116044646_create_viewcounts.rb
@@ -0,0 +1,10 @@
+class CreateViewcounts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :viewcounts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
